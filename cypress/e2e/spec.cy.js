86a149e7a91f0df54153eb022e9f00e18e6e9f35
Wahyu Saputro <49488222+wahyuridho@users.noreply.github.com>
2024-10-23 19:50:40 +0700

    Add negatif test case
    - User get error message when email and password not match

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,4 +2,8 @@ describe('Login Skenario', () => {
   it('Should succes login using valid credential', () => {
     cy.visit('https://kasirdemo.vercel.app/login')
   })
+
+  it('Should get Error Message when login using invalid credential', () => {
+    cy.visit('https://kasirdemo.vercel.app/login')
+  })
 })
