b90dee1631e591072b737cc32a7e0ab257ce8f89
putrisp <prikustyowati.putri@gmail.com>
2024-10-23 18:37:32 +0700

    update config

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,6 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    specPattern:"cypress/integration/**/*.{js,jsx,ts,tsx,feature}"
   },
 });
