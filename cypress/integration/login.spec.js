1c0055274654351c8a00c9eee96524fe953b95db
putrisp <prikustyowati.putri@gmail.com>
2024-10-24 20:38:51 +0700

    add fixture login data

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -2,11 +2,18 @@ import LoginPage from "../pages/login.page.js"
 import DashboardPage from "../pages/dashboard.page.js"
 
 describe('Login Skenario', () => {
+
+  before(function() {
+    cy.fixture('login').then(function(data) {
+      globalThis.data = data;
+    })
+  });
+
     it('Should succes login using valid credential', () => {
       LoginPage.open()
-      LoginPage.fillform('srituemail@test.com', 'Test123')
+      LoginPage.fillform(data.validEmail, data.validPwd)
       LoginPage.submit()
-      cy.url().should('eq', 'https://kasirdemo.vercel.app/dashboard')
+      cy.url().should('eq', 'https://kasirdemo.vercel.app/dashboard', {setTimeout: 5000})
       DashboardPage.userLogedIn.should('be.visible')
     })
   
